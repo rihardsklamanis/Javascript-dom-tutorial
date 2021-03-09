164f227829e03dffbecef0d9b1894971f81c01a0
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-09 16:37:23 +0200

    17 lesson

--- app.js
+++ app.js
@@ -56,7 +56,23 @@ searchBar.addEventListener('keyup', function(e){
             book.style.display = 'block';
         }else{
             book.style.display = 'none';
-        
         }
     })
+})
+
+//tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e){
+    if(e.target.tagName == "LI") {
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel){
+            if (panel == targetPanel){
+                panel.classList.add('active');
+            }else{
+                panel.classList.remove('active');
+            }
+        })
+
+    }
 })
