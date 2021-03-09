951f9ddefd37eaf99e73f8a7007cb14d1d231e90
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-09 15:19:19 +0200

    11 lesson

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const list = document.querySelector('#book-list ul')
 
+// delete books
 list.addEventListener('click', function(e){
     if(e.target.className == 'delete'){
         const li = e.target.parentElement;
         list.removeChild(li);
     }
+})
+
+// add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+
 })
