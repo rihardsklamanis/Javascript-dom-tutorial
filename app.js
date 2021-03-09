fdfe697a950f7c2c6a1c8c1cb0910bedb8593c37
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-09 14:35:06 +0200

    6 lesson

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-// 2 lesson
-//const search = document.getElementById('search-books');
-//const bookList = document.getElementById('book-list');
+const banner = document.querySelector('#page-banner')
 
-//console.log(search, bookList);
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-// 3 lesson
-//var titles = document.getElementsByClassName('title')
-//console.log(Array.isArray(titles))
-//console.log(Array.isArray(Array.from(titles)));
-//Array.from(titles).forEach(function(item){
-//console.log(item); 
-//});
-
-// 4 lesson
-//const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-//console.log(wmf);
-
-//var books = document.querySelector('#book-list li .name');
-//console.log(books);
-
-//books = document.querySelectorAll('#book-list li .name');
-//console.log(books);
-
-//Array.from(books).forEach(function(book){
-//    console.log(book);
-//});
-
-// 5 lesson
-var books = document.querySelectorAll('#book-list li .name');
-
-Array.from(books).forEach(function(book){
- book.textContent += '(book title)';
-});
-
-const bookList = document.querySelector('#book-list');
-//bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML</p>'
+const clonedBanner = banner.cloneNode(true)
+console.log(clonedBanner)
