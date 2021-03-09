069bfc041f96bbcbea517014ee5981d732ece2e4
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-09 14:08:21 +0200

    3 lession

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 
 console.log(search, bookList);
+
+var titles = document.getElementsByClassName('title')
+//console.log(Array.isArray(titles))
+//console.log(Array.isArray(Array.from(titles)));
+Array.from(titles).forEach(function(item){
+console.log(item); 
+})
